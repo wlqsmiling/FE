3290af198657907b886715d0adb8a054af05ec26
Liqun Wu <liqunwu@liqun-mini.local>
2024-03-05 19:18:59 +0800

    testtest

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+// import React from 'react';
+
+// function App() {
+//     return (
+//         <div>
+//             <h1>Hello, React!</h1>
+//             <p>This is a simple React application.</p>
+//         </div>
+//     );
+// }
+
+// export default App;
---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// ReactDOM.render(<div>
+//             <h1>Hello, React!</h1>
+//             <p>This is a simple React application.</p>
+//         </div>, document.getElementById('root'));
